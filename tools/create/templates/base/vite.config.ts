0f020743c4f0fbc9b303bb29c507da9cad9e3fd8
Cailen Fisher <cailen.fisher@gmail.com>
2026-05-24 02:05:20 -0400

    fix vite config for ssr (precompile raw svelte in libs)

--- tools/create/templates/base/vite.config.ts
+++ tools/create/templates/base/vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [sveltekit()],
   ssr: {
-    noExternal: [/^@sveltebuilder\//]
+    noExternal: [/^@sveltebuilder\//, 'bits-ui', 'svelte-toolbelt', 'runed']
   }
 })
